37c1e470cf5ed0dbdaf7015cab32599086edec1f
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-11 14:45:56 -0500

    setup framework for index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.hasMany(Tag, {
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(ProductTag, {
+  
+})
 
 module.exports = {
   Product,
