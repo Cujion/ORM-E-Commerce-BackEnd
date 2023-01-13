ca417d757fa741a6af3a53878eb4405d076ac016
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-12 19:19:46 -0500

    created post route to create a new  category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,13 +16,31 @@ router.get('/', async (req, res) => {
 });
 
 // find one category by its `id` value
-router.get('/:id', (req, res) => {
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create a new category
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
 });
 
 // update a category by its `id` value
